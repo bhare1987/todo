a56df2ddae240271c14228401b63ecb6c598269f
bhare1987 <brandonlhare@gmail.com>
2016-02-24 15:22:05 -0500

    updated refactor

--- app.js
+++ app.js
@@ -1,26 +1,101 @@
+$(document).ready(function(){
+  todoList.init();
+});
+
 var todoList = {
   init: function() {
     todoList.presentation();
     todoList.events();
   },
   presentation: function() {
-
+    todoList.addToDoListToDom(todoList.getToDo(), $('.todos'));
+    todoList.updateCount($('.todoMenuActive').text(), todoList.getToDo());
   },
   events: function() {
+    var $Selector = $('.todos');
+
+    $Selector.on("keypress", "input[name='todoInput']", function(event){
+      if (event.keyCode === 13) {
+        if ($(this).attr('id') ===  'editToDo' ) {
+          todoList.editToDo($(this).data('listitemidx'), $(this).val());
+          todoList.addToDoListToDom(todoList.getToDo(), $Selector);
+        } else if ($(this).attr('id') === 'addToDo') {
+          todoList.addToDo(todoList.getToDoFromDom(this));
+          todoList.addToDoListToDom(todoList.getToDo(), $Selector);
+        }
+      }
+      todoList.updateCount($('.todoMenuActive').text(), todoList.getToDo());
+    });
+
+    $Selector.on("dblclick", ".todoItem", function(event){
+      if ($('#editToDo').length === 0) {
+        var input = _.template(templates.todoEdit);
+        var content = $(this).find("span.todoText").text();
+        var idx = $(this).data('listitemidx');
+        $(this).replaceWith(input({idx:idx}));
+        $('#editToDo').val(content);
+      }
+    });
+
+    $Selector.on("click", ".checkbox", function(event){
+      var $item = $(this)
+      var idx = $item.parent().data("listitemidx");
+      if (todoList.getToDo(idx).complete === false) {
+        todoList.editToDo(idx, true);
+        todoList.addToDoListToDom(todoList.getFilter(todoList.getToDo()), $Selector);
+      } else {
+        todoList.editToDo(idx, false);
+        todoList.addToDoListToDom(todoList.getFilter(todoList.getToDo()), $Selector);
+      }
+      todoList.updateCount($('.todoMenuActive').text(), todoList.getToDo());
+    });
+
+    $('.todoMenu').on("click", "li", function(event){
+      $(this).siblings().removeClass('todoMenuActive');
+      $(this).addClass('todoMenuActive');
+      var text = $(this).text();
+      if (text === "All") {
+        todoList.addToDoListToDom(todoList.getToDo(), $Selector);
+        todoList.updateCount("All", todoList.getToDo());
+      } else if (text === "Active") {
+        var active = todoList.getFilter(todoList.getToDo());
+        todoList.addToDoListToDom(active, $Selector);
+        todoList.updateCount("Active", todoList.getToDo());
+      } else if (text === "Completed") {
+        var completed = todoList.getFilter(todoList.getToDo());
+        todoList.addToDoListToDom(completed, $Selector);
+        todoList.updateCount("Completed", todoList.getToDo());
+      }
+    });
+
+    $Selector.on("click", '.deleteToDo', function(event){
+      todoList.deleteToDo($(this).parent().data("listitemidx"));
+      todoList.addToDoListToDom(todoList.getToDo(), $Selector);
+      todoList.updateCount($('.todoMenuActive').text(),todoList.getToDo());
+    });
+
+    $('.clear').on("click", function(event){
+      var completed = todoList.getCompleted(todoList.getToDo());
+      completed.forEach(function(el, idx){
+        todoList.deleteToDo(idx);
+      })
+      todoList.addToDoListToDom(todoList.getToDo(), $Selector);
+      todoList.updateCount($('.todoMenuActive').text(),todoList.getToDo());
+    });
 
   },
   addToDo: function(newToDo) {
     return todos.push(newToDo);
   },
   getToDo: function(todoIdx) {
-    if (todoIdx) {
+    if (todoIdx === 0 || typeof todoIdx === "number") {
       return todos[todoIdx];
     } else {
     return todos;
     }
   },
   deleteToDo: function(todoIdx) {
-    return todos.splice(todoIdx, 1);
+    return delete(todos[todoIdx]);
   },
   editToDo: function(todoIdx, content, complete) {
      var args = [].slice.call(arguments);
@@ -42,222 +117,281 @@ var todoList = {
   constructTmpl: function(templateName){
     var tmpl = todoList.getTmpl(templateName);
     var tmplFunc = _.template(tmpl);
-    return tmplFunc();
+    return tmplFunc;
   },
   addItemToDom: function(templateName, item){
     var tmpl = todoList.constructTmpl(templateName);
-    return tmpl(item));
+    if (item) {
+      return tmpl(item);
+    } else {
+      return tmpl();
+    }
   },
-  addToDoListToDom: function(templateName, dataArray, $target){
+  addToDoListToDom: function(dataArray, target){
+    $(target).html('');
     var result = "";
-      result += addItemToDom()
-    var title = constructTmpl(templates.listTitle);
-    $container.html('');
-    $container.append(title());
-    arr.forEach(function(el, idx, arr){
+    result += todoList.addItemToDom('listTitle');
+    dataArray.forEach(function(el, idx, arr){
       if (!arr[0].idx){
         el.idx = idx;
       }
-      addItemToDom(el, templates.todo, $container);
+      result += todoList.addItemToDom('todo', el);
     });
-    $container.append(input());
-  }
-
-
-
-
-
-}
-
-$(document).ready(function(){
-  var $Selector = $('.todos');
-  addToDoListToDom(getToDo());
-  updateCount($('.todoMenuActive').html());
-  $Selector.on("keypress", "input[name='todoInput']", function(event){
-    if (event.keyCode === 13) {
-      if ($(this).attr('id') ===  'editToDo' ) {
-        editToDo($(this).data('listitemidx'), $(this).val());
-        addToDoListToDom(todos);
-      } else if ($(this).attr('id') === 'addToDo') {
-        addToDo(getToDoFromDom($(this)));
-        addToDoListToDom(todos);
+    result += todoList.addItemToDom('todoAdd');
+    $(target).append(result);
+  },
+  getToDoFromDom: function(selector){
+    var content = $(selector).val();
+    return {
+      content: content,
+      complete: false
+    }
+  },
+  updateCount: function(flag, dataArray) {
+    var numItems = 0;
+    if (flag === "Active") {
+      numItems = dataArray.filter(function(el){
+        return el.complete === false
+      }).length;
+      if (numItems === 1) {
+        $('.todoCount').text(numItems + " item left");
+      } else {
+        $('.todoCount').text(numItems + " items left");
+      }
+    } else if (flag === "Completed") {
+      numItems = dataArray.filter(function(el){
+        return el.complete === true
+      }).length;
+      if (numItems === 1) {
+        $('.todoCount').text(numItems + " item complete");
+      } else {
+        $('.todoCount').text(numItems + " items complete");
+      }
+    } else {
+      numItems = dataArray.length;
+      if (numItems === 1) {
+        $('.todoCount').text(numItems + " item");
+      } else {
+        $('.todoCount').text(numItems + " items");
       }
     }
-    updateCount($('.todoMenuActive').html());
-  });
-
-  $Selector.on("dblclick", ".todoItem", function(event){
-    if ($('#editToDo').length === 0) {
-      var input = _.template(templates.todoEdit);
-      var content = $(this).find("span.todoText").html();
-      var idx = $(this).data('listitemidx');
-      $(this).replaceWith(input({idx:idx}));
-      $('#editToDo').val(content);
-    }
-  });
-
-  $Selector.on("click", ".checkbox", function(event){
-    var $item = $(this)
-    var idx = $item.parent().data("listitemidx");
-    if (todos[idx].complete === false) {
-      editToDo(idx, true);
-      addToDoListToDom(getFilter(getToDo()))
-    } else {
-      editToDo(idx, false);
-      addToDoListToDom(getFilter(getToDo()))
-    }
-    updateCount($('.todoMenuActive').html());
-  });
-
-  $('.todoMenu').on("click", "li", function(event){
-    $(this).siblings().removeClass('todoMenuActive');
-    $(this).addClass('todoMenuActive');
-    if ($(this).html() === "All") {
-      addToDoListToDom(getToDo());
-      updateCount();
-    } else if ($(this).html() === "Active") {
-      var active = getFilter(todos);
-      addToDoListToDom(active);
-      updateCount("Active");
-    } else if ($(this).html() === "Completed") {
-      var completed = getFilter(todos);
-      addToDoListToDom(completed);
-      updateCount("Completed");
-    }
-  });
-
-  $Selector.on("click", '.deleteToDo', function(event){
-    deleteToDo($(this).parent().data("listitemidx"));
-    addToDoListToDom(getToDo());
-    updateCount($('.todoMenuActive').text());
-  });
-
-  $('.clear').on("click", function(event){
-    var completed = getCompleted();
-    completed.forEach(function(el, idx){
-      deleteToDo(idx);
-    })
-    addToDoListToDom(getFilter(getToDo()));
-    updateCount($('.todoMenuActive').text());
-  });
-
-});
-
-//CRUD Functions
-
-function addToDo(newToDo){
-  return todos.push(newToDo);
-}
-
-function getToDo(todoIdx){
-  if (todoIdx) {
-    return todos[todoIdx];
-  } else {
-  return todos;
-  }
-}
-
-function deleteToDo(todoIdx) {
-  return todos.splice(todoIdx, 1);
-}
-
-function editToDo(todoIdx, content, complete) {
-   var args = [].slice.call(arguments);
-   var todoItem = todos[todoIdx];
-   if (typeof args[1] === "boolean") {
-     todoItem.complete = content;
-   } else {
-     if (content) {
-       todoItem.content = content;
-     }
-     if (complete) {
-       todoItem.complete = complete;
-     }
-   }
-}
-
-//DOM Functions
-
-function addItemToDom(item, templateStr, $target){
-  var tmpl = _.template(templateStr);
-  $target.append(tmpl(item));
-}
-
-function addToDoListToDom(arr){
-  var $container = $('.todos');
-  var input = _.template(templates.todoAdd);
-  var title = _.template(templates.listTitle);
-  $container.html('');
-  $container.append(title());
-  arr.forEach(function(el, idx, arr){
-    if (!arr[0].idx){
-      el.idx = idx;
-    }
-    addItemToDom(el, templates.todo, $container);
-  });
-  $container.append(input());
-}
-
-function getToDoFromDom($selector){
-  var content = $selector.val();
-  return {
-    content: content,
-    complete: false
-  }
-}
-
-function updateCount(flag) {
-  var numItems = 0;
-  if (flag === "Active") {
-    numItems = todos.filter(function(el){
-      return el.complete === false
-    }).length;
-    if (numItems === 1) {
-      $('.todoCount').text(numItems + " item left");
-    } else {
-      $('.todoCount').text(numItems + " items left");
-    }
-  } else if (flag === "Completed") {
-    numItems = todos.filter(function(el){
-      return el.complete === true
-    }).length;
-    if (numItems === 1) {
-      $('.todoCount').text(numItems + " item complete");
-    } else {
-      $('.todoCount').text(numItems + " items complete");
-    }
-  } else {
-    numItems = todos.length;
-    if (numItems === 1) {
-      $('.todoCount').text(numItems + " item");
-    } else {
-      $('.todoCount').text(numItems + " items");
-    }
-  }
-}
-
-function getCompleted() {
-  var completed = todos.filter(function(el, idx, arr){
-    arr[idx].idx = idx;
-    return el.complete === true;
-  });
-  return completed;
-}
-
-function getFilter(arr) {
-  var result = [];
-  if ($('.todoMenuActive').text() === "Active") {
-    result = arr.filter(function(el, idx, arr){
+  },
+  getCompleted: function(dataArray) {
+    var completed = dataArray.filter(function(el, idx, arr){
       arr[idx].idx = idx;
-      return el.complete === false
+      return el.complete === true;
     });
-  } else if ($('.todoMenuActive').text() === "Completed") {
-    result = arr.filter(function(el, idx, arr){
-      arr[idx].idx = idx;
-      return el.complete === true
-    })
-  } else {
-    result = arr;
+    return completed;
+  },
+  getFilter: function(dataArray) {
+    var result = [];
+    var $selector = $('.todoMenuActive');
+    if ($selector.text() === "Active") {
+      result = dataArray.filter(function(el, idx, arr){
+        arr[idx].idx = idx;
+        return el.complete === false
+      });
+    } else if ($selector.text() === "Completed") {
+      result = dataArray.filter(function(el, idx, arr){
+        arr[idx].idx = idx;
+        return el.complete === true
+      })
+    } else {
+      result = dataArray;
+    }
+    return result;
   }
-  return result;
 }
+
+// $(document).ready(function(){
+//   var $Selector = $('.todos');
+//   addToDoListToDom(getToDo());
+//   updateCount($('.todoMenuActive').html());
+//   $Selector.on("keypress", "input[name='todoInput']", function(event){
+//     if (event.keyCode === 13) {
+//       if ($(this).attr('id') ===  'editToDo' ) {
+//         editToDo($(this).data('listitemidx'), $(this).val());
+//         addToDoListToDom(todos);
+//       } else if ($(this).attr('id') === 'addToDo') {
+//         addToDo(getToDoFromDom($(this)));
+//         addToDoListToDom(todos);
+//       }
+//     }
+//     updateCount($('.todoMenuActive').html());
+//   });
+//
+//   $Selector.on("dblclick", ".todoItem", function(event){
+//     if ($('#editToDo').length === 0) {
+//       var input = _.template(templates.todoEdit);
+//       var content = $(this).find("span.todoText").html();
+//       var idx = $(this).data('listitemidx');
+//       $(this).replaceWith(input({idx:idx}));
+//       $('#editToDo').val(content);
+//     }
+//   });
+//
+//   $Selector.on("click", ".checkbox", function(event){
+//     var $item = $(this)
+//     var idx = $item.parent().data("listitemidx");
+//     if (todos[idx].complete === false) {
+//       editToDo(idx, true);
+//       addToDoListToDom(getFilter(getToDo()))
+//     } else {
+//       editToDo(idx, false);
+//       addToDoListToDom(getFilter(getToDo()))
+//     }
+//     updateCount($('.todoMenuActive').html());
+//   });
+//
+//   $('.todoMenu').on("click", "li", function(event){
+//     $(this).siblings().removeClass('todoMenuActive');
+//     $(this).addClass('todoMenuActive');
+//     if ($(this).html() === "All") {
+//       addToDoListToDom(getToDo());
+//       updateCount();
+//     } else if ($(this).html() === "Active") {
+//       var active = getFilter(todos);
+//       addToDoListToDom(active);
+//       updateCount("Active");
+//     } else if ($(this).html() === "Completed") {
+//       var completed = getFilter(todos);
+//       addToDoListToDom(completed);
+//       updateCount("Completed");
+//     }
+//   });
+//
+//   $Selector.on("click", '.deleteToDo', function(event){
+//     deleteToDo($(this).parent().data("listitemidx"));
+//     addToDoListToDom(getToDo());
+//     updateCount($('.todoMenuActive').text());
+//   });
+//
+//   $('.clear').on("click", function(event){
+//     var completed = getCompleted();
+//     completed.forEach(function(el, idx){
+//       deleteToDo(idx);
+//     })
+//     addToDoListToDom(getFilter(getToDo()));
+//     updateCount($('.todoMenuActive').text());
+//   });
+//
+// });
+//
+// //CRUD Functions
+//
+// function addToDo(newToDo){
+//   return todos.push(newToDo);
+// }
+//
+// function getToDo(todoIdx){
+//   if (todoIdx) {
+//     return todos[todoIdx];
+//   } else {
+//   return todos;
+//   }
+// }
+//
+// function deleteToDo(todoIdx) {
+//   return todos.splice(todoIdx, 1);
+// }
+//
+// function editToDo(todoIdx, content, complete) {
+//    var args = [].slice.call(arguments);
+//    var todoItem = todos[todoIdx];
+//    if (typeof args[1] === "boolean") {
+//      todoItem.complete = content;
+//    } else {
+//      if (content) {
+//        todoItem.content = content;
+//      }
+//      if (complete) {
+//        todoItem.complete = complete;
+//      }
+//    }
+// }
+//
+// //DOM Functions
+//
+// function addItemToDom(item, templateStr, $target){
+//   var tmpl = _.template(templateStr);
+//   $target.append(tmpl(item));
+// }
+//
+// function addToDoListToDom(arr){
+//   var $container = $('.todos');
+//   var input = _.template(templates.todoAdd);
+//   var title = _.template(templates.listTitle);
+//   $container.html('');
+//   $container.append(title());
+//   arr.forEach(function(el, idx, arr){
+//     if (!arr[0].idx){
+//       el.idx = idx;
+//     }
+//     addItemToDom(el, templates.todo, $container);
+//   });
+//   $container.append(input());
+// }
+//
+// function getToDoFromDom($selector){
+//   var content = $selector.val();
+//   return {
+//     content: content,
+//     complete: false
+//   }
+// }
+//
+// function updateCount(flag) {
+//   var numItems = 0;
+//   if (flag === "Active") {
+//     numItems = todos.filter(function(el){
+//       return el.complete === false
+//     }).length;
+//     if (numItems === 1) {
+//       $('.todoCount').text(numItems + " item left");
+//     } else {
+//       $('.todoCount').text(numItems + " items left");
+//     }
+//   } else if (flag === "Completed") {
+//     numItems = todos.filter(function(el){
+//       return el.complete === true
+//     }).length;
+//     if (numItems === 1) {
+//       $('.todoCount').text(numItems + " item complete");
+//     } else {
+//       $('.todoCount').text(numItems + " items complete");
+//     }
+//   } else {
+//     numItems = todos.length;
+//     if (numItems === 1) {
+//       $('.todoCount').text(numItems + " item");
+//     } else {
+//       $('.todoCount').text(numItems + " items");
+//     }
+//   }
+// }
+//
+// function getCompleted() {
+//   var completed = todos.filter(function(el, idx, arr){
+//     arr[idx].idx = idx;
+//     return el.complete === true;
+//   });
+//   return completed;
+// }
+//
+// function getFilter(arr) {
+//   var result = [];
+//   if ($('.todoMenuActive').text() === "Active") {
+//     result = arr.filter(function(el, idx, arr){
+//       arr[idx].idx = idx;
+//       return el.complete === false
+//     });
+//   } else if ($('.todoMenuActive').text() === "Completed") {
+//     result = arr.filter(function(el, idx, arr){
+//       arr[idx].idx = idx;
+//       return el.complete === true
+//     })
+//   } else {
+//     result = arr;
+//   }
+//   return result;
+// }
